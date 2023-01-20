c2ecdf8bd008a0b61309737e21b5c8cc4555c4eb
MARVINAKI <kosakbig@gmial.com>
2023-01-20 19:59:28 +0500

    HW done

--- src/main/java/com/example/recapp/controllers/MainController.java
+++ src/main/java/com/example/recapp/controllers/MainController.java
@@ -1,5 +1,6 @@
 package com.example.recapp.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Страница приветсвия")
 public class MainController {
     @GetMapping
+    @Operation(summary = "Получить краткое описание")
     public String test() {
         return "Web-приложение для сайта рецептов";
     }
