51e3a465f3c3c6133214b5880ff126e358e14142
MARVINAKI <kosakbig@gmial.com>
2023-01-20 19:55:35 +0500

    initial commit

--- src/main/java/com/example/recapp/controllers/MainController.java
+++ src/main/java/com/example/recapp/controllers/MainController.java
@@ -1,9 +1,11 @@
 package com.example.recapp.controllers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Страница приветсвия")
 public class MainController {
     @GetMapping
     public String test() {
